8078949df4250a1065cdef87e8abefe0d9ce6f13
stevenppk <steventhomasdev@gmail.com>
2022-06-04 11:59:54 -0400

    Completed product Category API integration with react in the home page

--- src/api/api.js
+++ src/api/api.js
@@ -1,45 +1,6 @@
-import { setToken } from "../components/utls/Session";
-
-//User
-
-export function CreateAccount(userData) {
-  return fetch("../../user/register", {
-    method: "POST",
-    headers: {
-      'Content-Type': "application/json",
-      'Accept': "application/json",
-    },
-    body: JSON.stringify({ userData }),
-  }).then((data) => data.json());
-}
-
-export function LoginCheck(userData) {
-    return fetch("../../user/login", {
-      method: "POST",
-      headers: {
-        'Content-Type': "application/json",
-        'Accept': "application/json",
-      },
-      body: JSON.stringify({ userData }),
-    }).then((data) => data.json())
-    .then((data) => setToken(data.accessToken));
-  }
-
-  //Products
-
-  export function GetProducts(searchString) {
-    return fetch("../../product/details", {
-      method: "POST",
-      headers: {
-        'Content-Type': "application/json",
-        'Accept': "application/json",
-      },
-      body: JSON.stringify({ searchString }),
-    }).then((data) => data.json());
-  }
 
   export function GetProductsCat() {
-    return fetch("https://mxzy5rj0rc.execute-api.us-east-2.amazonaws.com/default/GetProductCategory", {
+    return fetch("https://u94lnqyv4e.execute-api.us-east-2.amazonaws.com/staging/", {
       method: "POST",
       headers: {
         'Content-Type': "application/json",
@@ -48,15 +9,3 @@ export function LoginCheck(userData) {
       body: JSON.stringify(),
     }).then((data) => data.json());
   }
-
-  export function GetProductsFilterCat(categoryCode) {
-    return fetch("../../product/filter", {
-      method: "POST",
-      headers: {
-        'Content-Type': "application/json",
-        'Accept': "application/json",
-      },
-      body: JSON.stringify({ categoryCode }),
-    }).then((data) => data.json());
-  }
-
--- src/components/homepage/product_categories/ProductCategories.jsx
+++ src/components/homepage/product_categories/ProductCategories.jsx
@@ -2,54 +2,51 @@ import React, { useState } from "react";
 import { GetProductsCat } from "../../../api/api";
 
 export default function ProductCategories() {
-  let [productCatList, setproductCatList] = useState();
-  GetProductsCat().then((data) => setproductCatList(data));
+  let [productCatList, setproductCatList] = useState("empty");
 
-  return (
-    <div>
-      <section id="new-arrivals" className="new-arrivals">
-        <div className="container">
-          <div className="section-header">
-            <h2>Product Categories</h2>
-          </div>
-          <div className="new-arrivals-content">
-            <div className="row">
-              {productCatList?.map((categories) => (
-                <div className="col-md-3 col-sm-4">
-                  <div className="single-new-arrival">
-                    <div className="single-new-arrival-bg">
-                      <img
-                        src={categories.image}
-                        alt={categories.cat}
-                      />
-                      <div className="single-new-arrival-bg-overlay"></div>
-                      <div className="sale bg-1">
-                        <p>sale</p>
+  if (productCatList == "empty") {
+    GetProductsCat().then((data) => setproductCatList(data.body));
+  }
+
+  if (productCatList != "empty") {
+    return (
+      <div>
+        <section id="new-arrivals" className="new-arrivals">
+          <div className="container">
+            <div className="section-header">
+              <h2>Product Categories</h2>
+            </div>
+            <div className="new-arrivals-content">
+              <div className="row">
+                {productCatList?.map((categories) => (
+                  <div className="col-md-3 col-sm-4">
+                    <div className="single-new-arrival">
+                      <div className="single-new-arrival-bg">
+                        <img src={categories.image} alt={categories.cat} />
+                        <div className="single-new-arrival-bg-overlay"></div>
+                        <div className="new-arrival-cart">
+                          <p>
+                            <span className="lnr"></span>
+                            <a href="#">
+                              View <span>all </span>
+                            </a>
+                          </p>
+                          <p className="arrival-review pull-right">
+                            <span className="lnr lnr-frame-expand"></span>
+                          </p>
+                        </div>
                       </div>
-                      <div className="new-arrival-cart">
-                        <p>
-                          <span className="lnr lnr-cart"></span>
-                          <a href="#">
-                            add <span>to </span> cart
-                          </a>
-                        </p>
-                        <p className="arrival-review pull-right">
-                          <span className="lnr lnr-heart"></span>
-                          <span className="lnr lnr-frame-expand"></span>
-                        </p>
-                      </div>
+                      <h4>
+                        <a href="#">{categories.cat}</a>
+                      </h4>
                     </div>
-                    <h4>
-                      <a href="#">wooden chair</a>
-                    </h4>
-                    <p className="arrival-product-price">$65.00</p>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
-  );
+        </section>
+      </div>
+    );
+  }
 }
